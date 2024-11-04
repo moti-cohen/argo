d7caa59867a1ab883d19468262808809b94d6414
moti-cohen <128600782+moti-cohen@users.noreply.github.com>
2024-11-04 12:55:08 +0200

    Update terrafom.tf

--- lambda-rds/terrafom.tf
+++ lambda-rds/terrafom.tf
@@ -1,13 +1,18 @@
-apiVersion: tf.galleybytes.com/v1alpha1
+apiVersion: tf.galleybytes.com/v1beta1
 kind: Terraform
 metadata:
   name: example-terraform-resource
   namespace: tf-system
 spec:
   terraformModule:
-    source: "git::https://github.com/moti-cohen/argo.git//lambda-rds"
+    source: "https://github.com/moti-cohen/argo.git"
     version: "v1.0.0"
-  variables:
-    - name: AWS_REGION
-      value: "eu-west-1"
-
+  backend: |
+    terraform {
+      backend "kubernetes" {
+        secret_suffix = "example-terraform"
+        namespace     = "tf-system"
+        in_cluster_config = true
+      }
+    }
+  terraformVersion: "1.3.5"
