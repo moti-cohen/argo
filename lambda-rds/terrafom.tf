d8ac7b58f95386b026ca1367d6380b2484845359
moti-cohen <128600782+moti-cohen@users.noreply.github.com>
2024-11-04 12:20:54 +0200

    Update terrafom.tf

--- lambda-rds/terrafom.tf
+++ lambda-rds/terrafom.tf
@@ -1,1 +1,13 @@
+apiVersion: tf.galleybytes.com/v1alpha1
+kind: Terraform
+metadata:
+  name: example-terraform-resource
+  namespace: tf-system
+spec:
+  terraformModule:
+    source: "https://github.com/moti-cohen/argo.git"
+    version: "v1.0.0"
+  variables:
+    - name: AWS_REGION
+      value: "eu-west-1"
 
