95e90acdcef1581465a177f50351040f224b39a8
moti-cohen <128600782+moti-cohen@users.noreply.github.com>
2024-12-23 17:27:44 +0200

    Update variables.tf

--- template/ec2/variables.tf
+++ template/ec2/variables.tf
@@ -16,18 +16,22 @@ variable "application" {
 variable "ec2_account_deployment" {
   description  = "retrieve the account id"
   type         = string
+  default      = "113834750184"
 }
 
  variable "ec2_region_deployment" {
   type         = string
+  default      = "eu-west-1"
 }
 
 variable "ec2_ami" {
   type         = string
+  default      = "ami-05842291b9a0bd79f"
 }
 
 variable "ec2_type" {
     type = string
+    default   = "t2.medium"
 }
 
 variable "ec2_name" {
@@ -37,4 +41,5 @@ variable "ec2_name" {
 
 variable "ec2_security_group" {
    type   = list(string)
+   default   = "sg-086cb401b675c8483"
 }
